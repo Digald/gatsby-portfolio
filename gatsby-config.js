ddfb9ce41700359cd620e5061d3a6be745589b7d
unknown <nosracer_gt@sbcglobal.net>
2019-09-28 23:15:00 -0500

    Added netlify headers plugin to help metrics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
             head: true,
         },
     },
+    'gatsby-plugin-netlify-headers',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
